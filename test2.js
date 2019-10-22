3948a0397e8aea019c12ee941e3537708688ff8c
kang.chen <kang.chen@coupang.com>
2019-10-22 15:21:14 +0800

    support show cpu usage

--- test2.js
+++ test2.js
@@ -1,28 +1,22 @@
-const { fork, execFileSync, spawn } = require('child_process');
-const { join } = require('path');
+var pidusage = require('pidusage');
+var usage = require('usage');
 
-var fs = require('fs'),
-  out = fs.openSync('./out1.log', 'a'),
-  err = fs.openSync('./out2.log', 'a');
+setInterval(function() {
+  pidusage('45319', function(err, stats) {
+    console.log(stats.memory / 1024 / 1024);
+    // => {
+    //   cpu: 10.0,            // percentage (from 0 to 100*vcore)
+    //   memory: 357306368,    // bytes
+    //   ppid: 312,            // PPID
+    //   pid: 727,             // PID
+    //   ctime: 867000,        // ms user + system time
+    //   elapsed: 6650000,     // ms since the start of the process
+    //   timestamp: 864000000  // ms since epoch
+    // }
+  });
 
-// console.log(join(__dirname, './test/app.js'));
-
-// spawn('node', [join(__dirname, './test.js')], {
-//   detached: true,
-//   stdio: ['ignore', out, err]
-// });
-
-spawn(
-  'node',
-  [
-    '/Users/kangchen/workspaces/node/easy-node-cluster/node_modules/forever/bin/monitor',
-    './test/app.js'
-  ],
-  {
-    detached: true,
-    stdio: ['ignore', out, err]
-  }
-);
-
-// stdio: ['ipc', 44, 45],
-//     detached: true
+  var pid = process.pid; // you can use any valid PID instead
+  usage.lookup('45317', function(err, result) {
+    console.log(result.memoryInfo.rss / 1024 / 1024);
+  });
+}, 1000);
